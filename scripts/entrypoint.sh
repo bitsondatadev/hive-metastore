ff049a7da9360ed5fb90ea6ec2d7254dfd01f113
pradeep <pradeep@localhost.localdomain>
2022-06-18 18:19:20 -0400

    add support postgres hive metastore

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,14 +3,11 @@
 export HADOOP_HOME=/opt/hadoop-3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export JAVA_HOME=/usr/local/openjdk-8
-export CLASSPATH="${HADOOP_CLASSPATH}:/opt/apache-hive-metastore-3.0.0-bin/lib/*.jar"
-
 export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}
 
 MYSQL='mysql'
 POSTGRES='postgres'
 
-echo "CLASSPATH = ${CLASSPATH}"
 if [ "${METASTORE_TYPE}" = "${MYSQL}" ]; then
   echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on 3306 ..."
   while ! nc -z ${METASTORE_DB_HOSTNAME} 3306; do
